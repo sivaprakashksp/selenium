7eefc137132d7f6bbd68ce65c886ab38e4ee22e2
sivaprakashksp <ksivaprakashsp@gmail.com>
2025-12-17 19:13:40 +0530

    Update LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -22,7 +22,7 @@ public class LoginTest {
     public void setup() {
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
+        //options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--window-size=1920,1080");
